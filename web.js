18952aa5a460f0f893cc88b5883b8c114edac339
bestape <user@bestape.net>
2013-07-10 02:16:01 +0000

    there's got to be a better way to test

--- web.js
+++ web.js
@@ -3,15 +3,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buffer;
-    fs.readFileSync('index.html', function (data){
-	buffer = new Buffer(data);
-	buffer.toString('utf-8');
-    });
-    response.send(buffer);
+    var buffer = new Buffer();
+    response.send(buffer.toString('utc-8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
