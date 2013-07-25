0a48f597dd04783bb97b1569a6c15ded16f26dae
bestape <user@bestape.net>
2013-07-25 19:49:19 +0000

    changed user list to JSON

--- web.js
+++ web.js
@@ -3,19 +3,31 @@ var fs = require('fs');
 var app = express();
 
 var users = function(user, pass){
-    if (user === 'snakes' && pass === 'inthegrass') {
-	return true;
+    var userList = {
+	"user": [{
+	    "username": "user1",
+	    "password": "helloworld"
+	}, {
+	    "username": "user2",
+	    "password": "helloworld"
+	}]
+    };
+    for (var i = 0; i < userList.user.length; i++) {
+	var potentialUsername = userList.user[i].username;
+	var potentialPassword = userList.user[i].password;
+	if (user === potentialUsername && pass === potentialPassword) {
+	    return true;
+	}
     }
-    else {
-	return false;
-    }
-}
+};
+
+var basicAuthMessage = "username and password please";
 
 app.use(express.basicAuth(function(user, pass) {
     var validUser = users(user, pass);
-    if (validUser) { return true }
-    else { return false } 
-}));    
+    if (validUser) { return true; }
+    else { return false; }
+}, basicAuthMessage));
 
 app.get('/', function(req, res) {
     var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'));
